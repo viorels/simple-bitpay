fce3e06b0b8d5cbdeb81bc2d6ac01e4c1972b666
Viorel Stirbu <viorels@gmail.com>
2014-11-03 11:46:11 +0200

    product page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Simple Pickup' });
+});
+
+var products = {
+  "go": {
+    "name": "Project GO",
+    "price": 40, // per month
+  },
+  "30": {
+    "name": "Simple 30",
+    "price": 399,
+  },
+}
+
+router.get('/product/:name', function(req, res) {
+  productName = products[req.params.name]["name"];
+  res.render('product', { title: productName});
 });
 
 module.exports = router;
