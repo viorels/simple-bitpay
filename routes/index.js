6edc2dafe300c102e24d256c12d1ebb7e14d466b
Viorel Stirbu <viorels@gmail.com>
2014-11-05 21:28:59 +0200

    create a new product and check for payment before showing it

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,69 @@
 var express = require('express');
 var router = express.Router();
 
+var bitpay = require('bitpay');
+var bitauth = require('bitauth');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Simple Pickup' });
 });
 
-var products = {
-  "go": {
-    "name": "Project GO",
-    "price": 40, // per month
-  },
-  "30": {
-    "name": "Simple 30",
-    "price": 399,
-  },
-}
+router.get('/product', function(req, res) {
+  var productName = req.query.name;
+  var productPrice = req.query.price;
+  if (!req.session.invoice) {
+    res.render('product', {
+      title: productName,
+      name: productName,
+      price: productPrice,
+    });
+  } else {
+    var encPrivKey = process.env.BITPAY_PRIVATE_KEY;
+    var privkey = bitauth.decrypt('', encPrivKey);
+    var client = bitpay.createClient(privkey);
 
-router.get('/product/:name', function(req, res) {
-  productName = products[req.params.name]["name"];
-  res.render('product', { title: productName});
+    client.on('ready', function() {
+      console.log("INVOICE", req.session.invoice);
+      client.get('invoices/' + req.session.invoice, function(err, invoice) {
+        console.log(err || invoice);
+        var paid = !err && (invoice.status == 'paid' || invoice.status == 'confirmed');
+        res.render('product', {
+          title: productName,
+          name: productName,
+          price: productPrice,
+          paid: paid,
+        });
+      });
+    });
+  }
+});
+
+router.post('/product', function(req, res) {
+  var productName = req.body.name;
+  var productPrice = req.body.price;
+
+  var port = req.app.settings.port;
+  var redirectURL = 'http://' + req.hostname  + (port == 80 || port == 443 ? '' : ':' + port) + '/product';
+  console.log('redirectURL', redirectURL);
+
+  var encPrivKey = process.env.BITPAY_PRIVATE_KEY;
+  var privkey = bitauth.decrypt('', encPrivKey);
+  var client = bitpay.createClient(privkey);
+
+  client.on('ready', function() {
+    client.post('invoices', {
+      itemDesc: productName,
+      price: productPrice,
+      // currency: 'USD',
+      currency: 'BTC',
+      redirectURL: redirectURL,
+    }, function(err, invoice) {
+      req.session.invoice = invoice.id
+      console.log(err || invoice);
+      res.redirect(303, invoice.url);
+    });
+  });
 });
 
 module.exports = router;
